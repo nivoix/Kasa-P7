7307eb50c00a3945d12021891c9d49bbab00e7ff
nivoix <jnivoix@yahoo.fr>
2023-03-31 14:25:40 +0200

    link logo Kasa

--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -7,9 +7,9 @@ import logo from '../assets/images/logonavbar.svg'
 function Navbar() {
   return (
     <nav className='navbar'>
-      <div className='navbar__logo'>
+      <NavLink to="/" className='navbar__logo'>
         <img src= {logo} alt='logo'></img>
-      </div>
+      </NavLink>
       <div className='navbar__link'>
         <NavLink to="/">Accueil</NavLink>
         <NavLink to="/about">A propos</NavLink>
